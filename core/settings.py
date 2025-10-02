9e079b2c675ab0b2b6e73a457896d12dae08d1f4
baxt <habibulaevhushbaht05@gmail.com>
2025-10-02 14:47:33 +0600

    aded static files

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static", "static_dirs"),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
