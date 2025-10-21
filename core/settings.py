1cbb2c2435931d3ff71e07101e1bc86e65a76430
Hushbaht Habibulaev <106618514+baxtdev@users.noreply.github.com>
2025-10-21 15:06:45 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "asdassdasd
+SECRET_KEY = "asdassdasd"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
